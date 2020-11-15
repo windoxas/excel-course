c95305ce64074bac5e197eb4c4e4f7da993d04a0
windoxas <xurshibekjon@mail.ru>
2020-11-15 21:21:53 +0500

    new modules

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import some from './module'
+import './scss/index.scss'
+console.log("working")
+
+some('Xurshid', 26)
---
+++ src/module.js
@@ -0,0 +1,5 @@
+function some(name,age){
+    console.log(`My name is ${name}, age ${age}`)
+}
+
+export  default some;
